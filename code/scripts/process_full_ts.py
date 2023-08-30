9f9642ccc9e38841859c3bf93a26f8dac47b4801
davidpritchard1 <davidp100@hotmail.co.uk>
2023-08-30 11:48:24 +0100

    updated for current thresholds being tested

--- code/scripts/process_full_ts.py
+++ code/scripts/process_full_ts.py
@@ -33,20 +33,26 @@ SCENARIO = args.scenario
 INDICATOR = args.var
 print(f'\nProcessing data into full time series {SCENARIO.lower()} and {INDICATOR}')
 
-agg_kwargs= {
+agg_kwargs = {
     f"{INDICATOR}_total": (INDICATOR, sum),
     f"{INDICATOR}_mean": (INDICATOR, np.mean),
+    f"{INDICATOR}_mean_total": (f"{INDICATOR}_mean", sum),
     f"{INDICATOR}_q50_total": (f"{INDICATOR}_q50", sum),
     f"{INDICATOR}_q75_total": (f"{INDICATOR}_q75", sum),
     f"{INDICATOR}_q90_total": (f"{INDICATOR}_q90", sum),
 }
+
 
 def get_full_ts(wah, monthly_los):
     wah["RZ_ID"] = wah['RZ_ID'].astype(int)
     monthly_los["RZ_ID"] = monthly_los['RZ_ID'].astype(int)
     full_ts = wah.merge(monthly_los, on=['RZ_ID', 'Year', 'Month'], how='inner')
     full_ts = full_ts.sort_values(by=['RZ_ID', 'Year', 'Month', 'LoS', 'buffer'])
-    full_ts = full_ts[['RZ_ID', 'Year', 'Month', 'LoS', 'buffer', f'{INDICATOR}_total', f'{INDICATOR}_mean', 'q50_anomaly_total', 'q75_anomaly_total', 'q90_anomaly_total', 'q50_deficit_total']]
+    full_ts = full_ts[[
+        'RZ_ID', 'Year', 'Month', 'LoS', 'buffer', f'{INDICATOR}_total', f'{INDICATOR}_mean',
+        'mean_anomaly_total', 'q50_anomaly_total',
+        'mean_deficit_total', 'q50_deficit_total', 'q75_deficit_total', 'q90_deficit_total'
+    ]]
     return full_ts
 
 
@@ -60,6 +66,9 @@ def main(config):
     monthly_los_melted = pd.read_csv(os.path.join(tempdir, SCENARIO.lower(), "monthly_los_melted.csv"))
     ENSEMBLES = utils.load_list(os.path.join(tempdir, SCENARIO.lower(), "ensembles"))
     YEARS = utils.load_list(os.path.join(tempdir, SCENARIO.lower(), "years"))
+
+    # TESTING
+    # wrz = wrz.loc[wrz['RZ_ID'] == 117]
 
     times = []
     for wrz_row in (pbar:=tqdm(wrz.itertuples(), total=len(wrz))):
@@ -83,18 +92,36 @@ def main(config):
                     wah_map = gpd.overlay(wah_coords, buffers_wrz, how='intersection')
                     wah_map = wah_map[['geometry', 'RZ_ID', 'buffer']].set_index('geometry')
                     wah_gdf = gpd.read_parquet(os.path.join(tempdir, SCENARIO.lower(), "by_basin", f"wrz_{wrz_row.RZ_ID}", f"{ensemble}.parquet"))
+
                     assert len(wah_gdf) > 0, "Basins data has zero rows."
                     wah_buffer = wah_gdf.join(wah_map, on='geometry', how='outer', lsuffix='_', rsuffix='_r')
-                    wah_buffer = wah_buffer[['RZ_ID', 'Year', 'Month', 'buffer', INDICATOR, f'{INDICATOR}_q50', f'{INDICATOR}_q75', f'{INDICATOR}_q90']].groupby(['RZ_ID', 'buffer', 'Year', 'Month']).agg(**agg_kwargs).reset_index()
+                    wah_buffer = wah_buffer[[
+                        'RZ_ID', 'Year', 'Month', 'buffer', INDICATOR, f'{INDICATOR}_mean', f'{INDICATOR}_q50',
+                        f'{INDICATOR}_q75', f'{INDICATOR}_q90'
+                    ]].groupby(['RZ_ID', 'buffer', 'Year', 'Month']).agg(**agg_kwargs).reset_index()
 
-                    # calculate metrics
-                    wah_buffer['q50_anomaly_total'] = wah_buffer[f'{INDICATOR}_total'] - wah_buffer[f'{INDICATOR}_q50_total']
-                    wah_buffer['q75_anomaly_total'] = wah_buffer[f'{INDICATOR}_total'] - wah_buffer[f'{INDICATOR}_q75_total']
-                    wah_buffer['q90_anomaly_total'] = wah_buffer[f'{INDICATOR}_total'] - wah_buffer[f'{INDICATOR}_q90_total']
-                    wah_buffer['q50_deficit_total'] = (wah_buffer[f'{INDICATOR}_q50_total'] - wah_buffer[f'{INDICATOR}_total']).apply(lambda x: np.max([x, 0.]))
+                    # calculate accumulated anomaly metrics
+                    wah_buffer['mean_anomaly_total'] = (
+                        wah_buffer[f'{INDICATOR}_total'] - wah_buffer[f'{INDICATOR}_mean_total']
+                    )
+                    wah_buffer['q50_anomaly_total'] = (
+                        wah_buffer[f'{INDICATOR}_total'] - wah_buffer[f'{INDICATOR}_q50_total']
+                    )
+
+                    # calculate accumulated deficit metrics
+                    for stat in ['mean', 'q50', 'q75', 'q90']:
+                        wah_buffer[f'{stat}_deficit_total'] = np.where(
+                            wah_buffer[f'{INDICATOR}_total'] - wah_buffer[f'{INDICATOR}_{stat}_total'] < 0.0,
+                            wah_buffer[f'{INDICATOR}_total'] - wah_buffer[f'{INDICATOR}_{stat}_total'],
+                            0.0
+                        )
 
                     # save
-                    wah_buffer = wah_buffer[['RZ_ID', 'Year', 'Month', 'buffer', f'{INDICATOR}_total', f'{INDICATOR}_mean', 'q50_anomaly_total', 'q75_anomaly_total', 'q90_anomaly_total', 'q50_deficit_total']]
+                    wah_buffer = wah_buffer[[
+                        'RZ_ID', 'Year', 'Month', 'buffer', f'{INDICATOR}_total', f'{INDICATOR}_mean',
+                        'mean_anomaly_total', 'q50_anomaly_total',
+                        'mean_deficit_total', 'q50_deficit_total', 'q75_deficit_total', 'q90_deficit_total',
+                    ]]
                     full_ts = get_full_ts(wah_buffer, monthly_los_melted[monthly_los_melted['ensemble'] == ensemble].copy())
                     full_ts.to_parquet(pathname_ts)
 
@@ -113,3 +140,4 @@ def main(config):
 if __name__ == "__main__":
     config = utils.load_config()
     main(config)
+
